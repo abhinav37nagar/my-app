505466c8efe3e31bdbe698d3577f87948b926cf8
abhinav <abhinav37nagar@gmail.com>
2023-02-06 18:54:31 +0530

    Installed Angular Material theming. Replaced list with material table.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule } from '@angular/material/table';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +13,15 @@ import { BooksComponent } from './books/books.component';
 
 @NgModule({
   declarations: [AppComponent, SubjectsComponent, BooksComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatSlideToggleModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DataSource } from '@angular/cdk/collections';
+import { Observable, ReplaySubject } from 'rxjs';
 
 import { Book } from '../book';
 import { BookService } from '../book.service';
@@ -12,6 +14,9 @@ export class BooksComponent {
   books: Book[] = [];
 
   constructor(private bookService: BookService) {}
+  displayedColumns: string[] = ['id','title', 'author', 'latest_publish_year', 'first_publish_year'];
+  dataToDisplay = [...this.books];
+  dataSource = new BooksDataSource(this.dataToDisplay);
 
   ngOnInit(): void {
     this.getBooks();
@@ -22,7 +27,7 @@ export class BooksComponent {
       for (let i = 0; i < books.length; i++) {
         if (i >= 10) break;
         let book: Book = {
-          id: i,
+          id: i+1,
           title:
             books[i].title +
             (books[i].subtitle ? `: ${books[i].subtitle}` : ''),
@@ -30,9 +35,28 @@ export class BooksComponent {
           latest_publish_year: Math.max(...books[i].publish_year),
           first_publish_year: books[i].first_publish_year,
         };
-        // console.log(books[i], book);
         this.books.push(book);
       }
+      this.dataSource.setData(this.books);
     });
+  }
+}
+
+class BooksDataSource extends DataSource<Book> {
+  private _dataStream = new ReplaySubject<Book[]>();
+
+  constructor(initialData: Book[]) {
+    super();
+    this.setData(initialData);
+  }
+
+  connect(): Observable<Book[]> {
+    return this._dataStream;
+  }
+
+  disconnect() {}
+
+  setData(data: Book[]) {
+    this._dataStream.next(data);
   }
 }
